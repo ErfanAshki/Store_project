8b9c5f79be9992c3d711d9e4511a5da4aa9764b3
erfan ashki <erfanbayern111@gmail.com>
2024-12-08 21:49:14 +0330

    some changes for django all auth

--- config/settings.py
+++ config/settings.py
@@ -59,9 +59,7 @@ AUTHENTICATION_BACKENDS = [
 
     'django.contrib.auth.backends.ModelBackend',
 
-    # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
-    ...
 ]
 
 MIDDLEWARE = [
@@ -168,3 +166,10 @@ LOGOUT_REDIRECT_URL = 'home'
 # crispy forms config
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# allauth config
+ACCOUNT_SESSION_REMEMBER = False
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_UNIQUE_EMAIL = True
