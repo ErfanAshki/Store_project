5f117e2b358c950a5622c47c4d5b74a81bafe1e3
erfan ashki <erfanbayern111@gmail.com>
2024-12-10 16:20:52 +0330

    add staic files and media files

--- products/models.py
+++ products/models.py
@@ -3,12 +3,17 @@ from django.shortcuts import reverse
 
 
 class Product(models.Model):
+    CATEGORY_CHOICES = (
+        ('Men shoes', 'Men shoes'),
+        ('Women shoes', 'Women shoes')
+    )
     title = models.CharField(max_length=150)
     body = models.TextField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=20)
     price = models.PositiveIntegerField(default=100)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
-    # image
+    image = models.ImageField(upload_to='shoes_pic')
     is_pay = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
 
