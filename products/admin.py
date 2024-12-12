920452499e557b8356015d04db22f0e0a3cfbe46
erfan ashki <erfanbayern111@gmail.com>
2024-12-12 15:59:52 +0330

    add CommentInlines to admin

--- products/admin.py
+++ products/admin.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 from .models import Product, Comment
 
 
+class CommentInline(admin.StackedInline):
+    model = Comment
+    fields = ['id', 'text', 'author', 'product', 'stars', 'active']
+    extra = 1
+
+
 class ProductAdmin(admin.ModelAdmin):
     ordering = ['-id']
     list_display = ['id', 'title', 'price', 'category', 'is_pay', 'active', 'datetime_created']
+    inlines = [CommentInline, ]
 
 
 admin.site.register(Product, ProductAdmin)
