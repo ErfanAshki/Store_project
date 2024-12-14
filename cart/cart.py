520ac5a4d88e063d2231f10dba45562b8c2ebbf1
erfan ashki <erfanbayern111@gmail.com>
2024-12-14 17:15:07 +0330

    implement iter - len - clear - get_total_price methods

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,4 @@
-
+from products.models import Product
 
 
 class Cart:
@@ -34,5 +34,29 @@ class Cart:
             del self.cart['product_id']
             self.save()
 
- 
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
 
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product_obj'] = product
+
+        for item in cart.values():
+            yield item
+
+    def __len__(self):
+        return len(self.cart.keys())
+
+    def clear(self):
+        del self.session['cart']
+        self.save()
+
+    def get_total_price(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        return sum(product.price for product in products)
+
+        
