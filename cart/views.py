d51b40e992d08f767ef14249a4048e27f916ee74
erfan ashki <erfanbayern111@gmail.com>
2024-12-17 18:38:57 +0330

    add messages and gettext & requred_POST decorator

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST, require_GET
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 from .cart import Cart
 from .forms import AddToCartProductForm
@@ -17,6 +20,7 @@ def cart_detail_view(request):
     return render(request, 'cart/cart.html', {'cart': cart})
 
 
+@require_POST
 def add_to_cart_view(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
@@ -28,6 +32,7 @@ def add_to_cart_view(request, product_id):
         quantity = cleaned_data['quantity']
         replace_current_quantity = cleaned_data['inplace']
         cart.add(product, quantity, replace_current_quantity)
+        messages.success(request, _('Product successfully added to cart.'))
 
         return redirect('cart:cart_detail')
 
@@ -37,6 +42,7 @@ def remove_from_cart_view(request, product_id):
     product = get_object_or_404(Product, id=product_id)
 
     cart.remove(product)
+    messages.error(request, _('Product successfully removed from cart.'))
 
     return redirect('cart:cart_detail')
 
@@ -44,5 +50,6 @@ def remove_from_cart_view(request, product_id):
 def clear_cart_view(request):
     cart = Cart(request)
     cart.clear()
+    messages.warning(request, _('Cart was cleared.'))
 
     return redirect('product_list')
