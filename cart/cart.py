2c1e37c86d4cdfc7c7f2daf59e638a3bb584200b
erfan ashki <erfanbayern111@gmail.com>
2024-12-17 11:33:26 +0330

    implement add to cart and fix product_id error in cart

--- cart/cart.py
+++ cart/cart.py
@@ -13,14 +13,26 @@ class Cart:
             cart = self.session['cart']
 
         self.cart = cart
+        # self.clean_invalid_keys()
+        # self.clear_session()
+
+    def clear_session(self):
+        del self.session['cart']
+        self.save()
+
+    def clean_invalid_keys(self):
+        invalid_keys = [key for key in self.cart.keys() if not key.isdigit()]
+        for key in invalid_keys:
+            del self.cart[key]
+        self.save()
 
     def add(self, product, quantity=1):
         product_id = str(product.id)
 
         if product_id not in self.cart:
-            self.cart['product_id'] = {'quantity': quantity}
+            self.cart[product_id] = {'quantity': quantity}
         else:
-            self.cart['product_id']['quantity'] += quantity
+            self.cart[product_id]['quantity'] += quantity
 
         self.save()
 
@@ -31,7 +43,7 @@ class Cart:
         product_id = str(product.id)
 
         if product_id in self.cart:
-            del self.cart['product_id']
+            del self.cart[product_id]
             self.save()
 
     def __iter__(self):
