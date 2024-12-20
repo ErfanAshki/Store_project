1b2a11a2e70eaf4960da036a1d5b5e7eaf739853
erfan ashki <erfanbayern111@gmail.com>
2024-12-20 22:49:55 +0330

    complete order_create_view

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext as _
+from django.contrib import messages
 
+from cart.cart import Cart
 from .forms import OrderForm
 from .models import Order, OrderItem
 
@@ -8,6 +11,12 @@ from .models import Order, OrderItem
 @login_required
 def order_create_view(request):
     order_form = OrderForm()
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        messages.warning(request, _('Your cart is empty , please add some product to it.'))
+        return redirect('product_list')
+
     if request.method == 'POST':
         order_form = OrderForm(request.POST)
         if order_form.is_valid():
@@ -15,6 +24,19 @@ def order_create_view(request):
             order_obj.user = request.user
             order_obj.save()
 
+            for item in cart:
+                product = item['product_obj']
+
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=product,
+                    quantity=item['quantity'],
+                    price=product.price,
+                )
+            cart.clear()
+
+            request.user.first_name = order_obj.first_name
+            request.user.last_name = order_obj.last_name
+            request.user.save()
+
     return render(request, 'orders/order.html', {'order_form': order_form})
-
-
